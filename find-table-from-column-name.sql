5475b426e251f4496b602cda5f449e914204e792
James Robert Lund III <35306013+lundjrl@users.noreply.github.com>
2025-02-03 13:50:44 -0500

    Create find-table-from-column-name.sql

---
+++ find-table-from-column-name.sql
@@ -0,0 +1,7 @@
+SELECT      c.name  AS 'ColumnName'
+            ,(SCHEMA_NAME(t.schema_id) + '.' + t.name) AS 'TableName'
+FROM        sys.columns c
+JOIN        sys.tables  t   ON c.object_id = t.object_id
+WHERE       c.name LIKE '%<Your column name here>%'
+ORDER BY    TableName
+            ,ColumnName;
