41b61d3e9b2a54ece7bd541d4e76dc3228724f72
Noah Jahn <36347998+noahjahn@users.noreply.github.com>
2025-04-19 16:40:55 -0400

    Add vscode formatter and format all documents (#9)

--- scripts/mssql/find-table-from-column-name.sql
+++ scripts/mssql/find-table-from-column-name.sql
@@ -1,7 +1,11 @@
-SELECT      c.name  AS 'ColumnName'
-            ,(SCHEMA_NAME(t.schema_id) + '.' + t.name) AS 'TableName'
-FROM        sys.columns c
-JOIN        sys.tables  t   ON c.object_id = t.object_id
-WHERE       c.name LIKE '%<Your column name here>%'
-ORDER BY    TableName
-            ,ColumnName;
+SELECT
+    c.name AS 'ColumnName',
+    (SCHEMA_NAME(t.schema_id) + '.' + t.name) AS 'TableName'
+FROM
+    sys.columns c
+    JOIN sys.tables t ON c.object_id = t.object_id
+WHERE
+    c.name LIKE '%<Your column name here>%'
+ORDER BY
+    TableName,
+    ColumnName;
